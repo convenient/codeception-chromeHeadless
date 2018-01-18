7c89021fda4bd2723616b1c203c4ced140ee321b
João Paulo Miranda <jpmirandas@gmail.com>
2018-01-17 22:03:59 -0300

    active codeception recorder extension

--- tests/functional/FirstCest.php
+++ tests/functional/FirstCest.php
@@ -2,10 +2,10 @@
 
 
 class FirstCest {
-    // tests
+
+
     public function tryToTest(FunctionalTester $I) {
         $I->amOnPage("/");
-        sleep(5);
-        $I->seeElement("#sb_ifc0");
+        $I->see("Imagens");
     }
 }
