288f415d47535cae98d61a3e1f003b2c78143a0e
João Paulo Miranda <jpmirandas@gmail.com>
2018-01-17 23:14:10 -0300

    unzip overwrites the old file

--- init.sh
+++ init.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 wget -N https://chromedriver.storage.googleapis.com/2.32/chromedriver_mac64.zip -P ./drivers/
-unzip ./drivers/chromedriver_mac64.zip -d ./drivers/
+unzip -o ./drivers/chromedriver_mac64.zip -d ./drivers/
 rm ./drivers/chromedriver_mac64.zip
