445c8c5f894036d8636fd31f736f6bfe8e8bdfb8
Luke Rodgers <lr@amp.co>
2018-11-25 19:47:00 +0000

    Try travis configuration

--- init.sh
+++ init.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 set -e
 
-wget -N https://chromedriver.storage.googleapis.com/2.32/chromedriver_mac64.zip -P ./drivers/
-unzip -o ./drivers/chromedriver_mac64.zip -d ./drivers/
-rm ./drivers/chromedriver_mac64.zip
+wget -N https://chromedriver.storage.googleapis.com/2.32/chromedriver_linux64.zip -P ./drivers/
+unzip -o ./drivers/chromedriver_linux64.zip -d ./drivers/
+rm ./drivers/chromedriver_linux64.zip
 
 if pgrep chromedriver; then
     printf "\033[92mKilling running chromedriver\n\033[0m";
