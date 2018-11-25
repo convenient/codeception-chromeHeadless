4e610bb2e707a44cefeac2b956df187274a52919
Luke Rodgers <lr@amp.co>
2018-11-25 19:34:44 +0000

    Try travis configuration

--- init.sh
+++ init.sh
@@ -1,5 +1,17 @@
 #!/bin/bash
+set -e
 
 wget -N https://chromedriver.storage.googleapis.com/2.32/chromedriver_mac64.zip -P ./drivers/
 unzip -o ./drivers/chromedriver_mac64.zip -d ./drivers/
 rm ./drivers/chromedriver_mac64.zip
+
+if pgrep chromedriver; then
+    printf "\033[92mKilling running chromedriver\n\033[0m";
+    pkill -f chromedriver;
+fi
+
+./drivers/chromedriver --url-base=/wd/hub &
+
+vendor/bin/codecept build -vvv
+
+set +e
--- tests/functional/FirstCest.php
+++ tests/functional/FirstCest.php
@@ -6,6 +6,6 @@ class FirstCest {
 
     public function tryToTest(FunctionalTester $I) {
         $I->amOnPage("/");
-        $I->see("Imagens");
+        $I->see("This domain is established to be used for illustrative examples in documents");
     }
 }
